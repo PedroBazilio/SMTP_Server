8a984e532efc5cad367f4d37f6bb14100ae3f5c9
PedroBazilio <pedrobazilio22@gmail.com>
2021-03-22 23:19:24 -0300

    criando "inbox" para cada nome dado no arquivo

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
- 
+import os
+
+#poe os usuarios em uma lista
+
+users = list()        
+with open ("NomesDeUsuarios.txt", "r") as user:
+    for line in user:
+        users.append(line.strip());
+
+
+
+#faz uma caixa de entrada para cada usuario
+
+for i in range(len(users)):
+    user = users[i]
+    file = open(str(user)+".txt","w");
+    file.close();
+
