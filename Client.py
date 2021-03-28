b38168606a393dcdb0fcbcf6f477027818daf477
PedroBazilio <pedrobazilio22@gmail.com>
2021-03-28 18:07:38 -0300

    cliente att

--- Client.py
+++ Client.py
@@ -6,21 +6,28 @@ serverPort = 25
 #cria socket
 
 socketClient = socket(AF_INET, SOCK_DGRAM)
-
-helo = input("Digite o comando helo + username para iniciar a transação\n")
-
-socketClient.sendto(helo.encode(), (server, serverPort) )
-
-respHelo, addr = socketClient.recvfrom(2048)
-if respHelo == "501 Syntax: HELO hostname":
+while 1:
     helo = input("Digite o comando helo + username para iniciar a transação\n")
 
     socketClient.sendto(helo.encode(), (server, serverPort) )
 
     respHelo, addr = socketClient.recvfrom(2048)
-else:
-    print(respHelo)
+    respHel = respHelo.decode("UTF-8")
+    if respHel == "501 Syntax: HELO hostname":
+        helo = input("Digite o comando helo + username para iniciar a transação\n")
 
-#
+        socketClient.sendto(helo.encode(), (server, serverPort) )
+
+        respHelo, addr = socketClient.recvfrom(2048)
+    else:
+        print(respHel)
+        print("\n")
+        mailFrom = input("Mail From: ")
+        socketClient.sendto(mailFrom.encode(), (server, serverPort))               
+        respMailFrom, addr = socketClient.recvfrom(2048)
+        respMailFro = respMailFrom.decode("UTF-8")
+        if(respMailFro == "")
+    
+    #
 
 socketClient.close()
